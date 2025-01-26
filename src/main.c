7e759e7818fc5ab6a6b6d1826cef9c7bd19fe1cd
Maarten Buis <buismaarten@outlook.com>
2025-01-26 22:43:20 +0100

    wip

--- src/main.c
+++ src/main.c
@@ -9,12 +9,13 @@
 #define OP_C(inst)           (((inst) & 0b11))
 #define OP_C_FUNCT3(inst)    (((inst) & 0b1110000000000000) >> 13)
 #define OP_C_FUNCT4(inst)    (((inst) & 0b1111000000000000) >> 12)
+#define OP_C_RS1(inst)       (((inst) & 0b0000111110000000) >> 7)
 #define OP_C_RS2(inst)       (((inst) & 0b0000000001111100) >> 2)
 #define OP_C_ADD(inst)       (OP_C(inst) == 0b10 && OP_C_FUNCT4(inst) == 0b1001 && OP_C_RS2(inst) != 0b00000)
-#define OP_C_ADDI(inst)      (OP_C(inst) == 0b01 && OP_C_FUNCT3(inst) == 0b000)
+#define OP_C_ADDI(inst)      (OP_C(inst) == 0b01 && OP_C_FUNCT3(inst) == 0b000 && OP_C_RS1(inst) != 0b00000)
 #define OP_C_ADDI16SP(inst)  (0) // TODO
 #define OP_C_ADDI4SPN(inst)  (0) // TODO
-#define OP_C_ADDIW(inst)     (0) // TODO
+#define OP_C_ADDIW(inst)     (OP_C(inst) == 0b01 && OP_C_FUNCT3(inst) == 0b001 && OP_C_RS1(inst) != 0b00000)
 #define OP_C_ADDW(inst)      (OP_C(inst) == 0b01 && OP_C_FUNCT4(inst) == 0b1001)
 #define OP_C_AND(inst)       (0) // TODO
 #define OP_C_ANDI(inst)      (0) // TODO
@@ -30,7 +31,7 @@
 #define OP_C_FSW(inst)       (0) // TODO
 #define OP_C_FSWSP(inst)     (0) // TODO
 #define OP_C_J(inst)         (0) // TODO
-#define OP_C_JAL(inst)       (OP_C(inst) == 0b01 && OP_C_FUNCT3(inst) == 0b001)
+#define OP_C_JAL(inst)       (OP_C(inst) == 0b01 && OP_C_FUNCT3(inst) == 0b001 && OP_C_RS1(inst) == 0b00000)
 #define OP_C_JALR(inst)      (OP_C(inst) == 0b10 && OP_C_FUNCT4(inst) == 0b1001 && OP_C_RS2(inst) == 0b00000)
 #define OP_C_JR(inst)        (0) // TODO
 #define OP_C_LD(inst)        (0) // TODO
