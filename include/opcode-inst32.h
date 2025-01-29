c81e5680261a0d1d17d9ebd572abbe33aaaa4855
Maarten Buis <buismaarten@outlook.com>
2025-01-29 21:11:14 +0100

    wip

--- include/opcode-inst32.h
+++ include/opcode-inst32.h
@@ -3,29 +3,23 @@
 #define OP_MASK(inst, s, w)  (((inst) >> (s)) & ((1U << (w)) - 1))
 
 #define OP_I_ADDI(inst) \
-    (OP_MASK(inst, 0, 2) == 0b11 && \
-     OP_MASK(inst, 2, 5) == 0b00100 && \
+    (OP_MASK(inst, 0, 7) == 0b0010011 && \
      OP_MASK(inst, 12, 3) == 0b000)
 
 #define OP_I_ADDIW(inst) \
-    (OP_MASK(inst, 0, 2) == 0b11 && \
-     OP_MASK(inst, 2, 5) == 0b00110 && \
+    (OP_MASK(inst, 0, 7) == 0b0011011 && \
      OP_MASK(inst, 12, 3) == 0b000)
 
 #define OP_I_ADDW(inst) \
-    (OP_MASK(inst, 0, 2) == 0b11 && \
-     OP_MASK(inst, 2, 5) == 0b01110 && \
+    (OP_MASK(inst, 0, 7) == 0b0111011 && \
      OP_MASK(inst, 12, 3) == 0b000)
 
 #define OP_I_ANDI(inst) \
-    (OP_MASK(inst, 0, 2) == 0b11 && \
-     OP_MASK(inst, 2, 5) == 0b00100 && \
+    (OP_MASK(inst, 0, 7) == 0b0010011 && \
      OP_MASK(inst, 12, 3) == 0b111)
 
 #define OP_I_AUIPC(inst) \
-    (OP_MASK(inst, 0, 2) == 0b11 && \
-     OP_MASK(inst, 2, 5) == 0b00101)
+    (OP_MASK(inst, 0, 7) == 0b0010111)
 
 #define OP_I_JAL(inst) \
-    (OP_MASK(inst, 0, 2) == 0b11 && \
-     OP_MASK(inst, 2, 5) == 0b11011)
+    (OP_MASK(inst, 0, 7) == 0b1101111)
