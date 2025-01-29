e2ed76f17410496e4e29a266776c97c21b799f5a
Maarten Buis <buismaarten@outlook.com>
2025-01-29 20:53:04 +0100

    wip

--- src/test-inst32.c
+++ src/test-inst32.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 
 static uint32_t program32[] = {
+    /* ADDI */  0x00178713,
+    /* ADDI */  0x01650513,
+    /* ADDI */  0x01850513,
+    /* ADDI */  0x01C50513,
+    /* ADDI */  0x02070713,
     /* AUIPC */ 0x00000517,
     /* AUIPC */ 0x00000717,
     /* AUIPC */ 0x00010117,
@@ -12,6 +17,11 @@ void test_inst32() {
     for (uint32_t i = 0; i < sizeof(program32) / sizeof(program32[0]); i++) {
         uint32_t inst = program32[i];
         uint32_t count = 0;
+
+        if (OP_I_ADDI(inst)) {
+            printf("Matched ADDI instruction 0x%08X\n", inst);
+            count++;
+        }
 
         if (OP_I_AUIPC(inst)) {
             printf("Matched AUIPC instruction 0x%08X\n", inst);
