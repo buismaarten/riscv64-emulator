7ff8b2035c08642c9fe7de7ac5a8d46807eb69ca
Maarten Buis <buismaarten@outlook.com>
2025-01-29 20:51:28 +0100

    wip

--- include/opcode-inst32.h
+++ include/opcode-inst32.h
@@ -2,15 +2,15 @@
 
 #define OP_MASK(inst, s, w)  (((inst) >> (s)) & ((1U << (w)) - 1))
 
-#define OP_I_ADDI(inst) \
-    (OP_MASK(inst, 0, 2) == 0b11 && \
-     OP_MASK(inst, 2, 5) == 0b00100 && \
-     OP_MASK(inst, 12, 3) == 0b000)
+// #define OP_I_ADDI(inst) \
+//     (OP_MASK(inst, 0, 2) == 0b11 && \
+//      OP_MASK(inst, 2, 5) == 0b00100 && \
+//      OP_MASK(inst, 12, 3) == 0b000)
 
 #define OP_I_AUIPC(inst) \
     (OP_MASK(inst, 0, 2) == 0b11 && \
      OP_MASK(inst, 2, 5) == 0b00101)
 
-#define OP_I_JAL(inst) \
-    (OP_MASK(inst, 0, 2) == 0b11 && \
-     OP_MASK(inst, 2, 5) == 0b11011)
+// #define OP_I_JAL(inst) \
+//     (OP_MASK(inst, 0, 2) == 0b11 && \
+//      OP_MASK(inst, 2, 5) == 0b11011)
--- src/test-inst32.c
+++ src/test-inst32.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 static uint32_t program32[] = {
-    //
+    /* AUIPC */ 0x00000517,
+    /* AUIPC */ 0x00000717,
+    /* AUIPC */ 0x00010117,
 };
 
 void test_inst32() {
@@ -11,7 +13,10 @@ void test_inst32() {
         uint32_t inst = program32[i];
         uint32_t count = 0;
 
-        //
+        if (OP_I_AUIPC(inst)) {
+            printf("Matched AUIPC instruction 0x%08X\n", inst);
+            count++;
+        }
 
         if (count == 0) {
             printf("Error: no match for instruction 0x%08X\n", inst);
